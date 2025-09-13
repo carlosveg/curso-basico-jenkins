f67e894468af453c5af1d1ed0c4e60c4e873b4ca
carlos vega <carlosvegapro@gmail.com>
2025-09-13 13:23:43 -0600

    new branch

--- first-api-rest/src/main/java/com/first/api/controller/HelloController.java
+++ first-api-rest/src/main/java/com/first/api/controller/HelloController.java
@@ -36,8 +36,20 @@ public class HelloController {
 
 
     //Otro comentario
+    @GetMapping("/hello5")
+    public String saludar5(){
+        return "Hola Mundo con REST en Spring Boot";
+    }
+
+    //Otro comentario
     @GetMapping("/hello6")
     public String saludar6(){
+        return "Hola Mundo con REST en Spring Boot";
+    }
+
+    //Otro comentario
+    @GetMapping("/hello7")
+    public String saludar7(){
         return "Hola Mundo con REST en Spring Boot";
     }
 }
